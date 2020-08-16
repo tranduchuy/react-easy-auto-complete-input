a9b911a725fe37c0041715cc174f29e64899e458
HuyTD <thewish.it@gmail.com>
2020-08-16 18:12:31 +0900

    Updates

---
+++ precache-manifest.36bcc1bf836f947e41c205127d5100c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9d394d786f1909c6bebf6acf6bbbdf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b1171f2ee928344a55e5",
+    "url": "./static/css/main.b9e13302.chunk.css"
+  },
+  {
+    "revision": "e5310a21b2068c0b8e80",
+    "url": "./static/js/2.0fba8792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0fba8792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1171f2ee928344a55e5",
+    "url": "./static/js/main.a4548cbb.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.a4548cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a178835bd3ccf83052",
+    "url": "./static/js/runtime-main.b06470a0.js"
+  }
+]);
